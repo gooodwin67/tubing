d7d39bc82919defbeb1cee5864784f9f8caaa1e1
sn67@inbox.ru <sn67@inbox.ru>
2025-01-28 21:50:17 +0300

    dep

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 export default defineConfig({
   plugins: [topLevelAwait()],
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
